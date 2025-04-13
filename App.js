0d272a955e4fb49e9711dc1fb83e9ec73ec6d07a
Alex Lozano <alexlozanodev@gmail.com>
2025-04-13 17:17:26 -0600

    Se creo la base de la aplicacion y arquitectura

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@eva-design/eva';
+import { SafeAreaView } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <IconRegistry icons={EvaIconsPack} /> */}
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <AppNavigator />
+        </SafeAreaView>
+      </ApplicationProvider>
+    </>
   );
 }
 
---
+++ src/screens/Home.js
@@ -0,0 +1,10 @@
+import { Layout, Text } from '@ui-kitten/components'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>Finanzas App</Text>
+    </Layout>
+  )
+}
